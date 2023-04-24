a0dd5a3b1d6a4afe1ec329c14f98c0ae5e120b1a
240763 <240763@edu.p.lodz.pl>
2023-04-24 19:17:30 +0200

    Revert "11 -Dokonano hermetyzacji nieprywatnych atrybutów"
    This reverts commit 624dd3edaf4d364dbd36c58e0967ac844962a13f.

--- src/main/java/edu/kis/vh/nursery/list/Node.java
+++ src/main/java/edu/kis/vh/nursery/list/Node.java
@@ -3,29 +3,10 @@ package edu.kis.vh.nursery.list;
 public class Node {
 
     protected final int VALUE;
-    protected Node prev, next;
+    public Node prev, next;
 
     public Node(int i) {
         VALUE = i;
     }
 
-    public int getVALUE() {
-        return VALUE;
-    }
-
-    public Node getPrev() {
-        return prev;
-    }
-
-    public void setPrev(Node prev) {
-        this.prev = prev;
-    }
-
-    public Node getNext() {
-        return next;
-    }
-
-    public void setNext(Node next) {
-        this.next = next;
-    }
 }
