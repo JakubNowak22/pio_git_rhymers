aa22bec43b66eb91d2ebe7459ee2b65d77dd5c44
240763 <240763@edu.p.lodz.pl>
2023-04-24 19:17:42 +0200

    Revert "10 - Wygenerowano getter dla pola total"
    This reverts commit 77b58a10d23d0029b286df7039d8805599f6abf7.

--- src/main/java/edu/kis/vh/nursery/DefaultCountingOutRhymer.java
+++ src/main/java/edu/kis/vh/nursery/DefaultCountingOutRhymer.java
@@ -6,12 +6,8 @@ public class DefaultCountingOutRhymer {
     private final int ERR_CODE = -1;
     private final int FULL = 11;
     private final int EMPTY = -1;
-    
-    private int total = -1;
 
-    public int getTotal() {
-        return total;
-    }
+    private int total = -1;
     public void countIn(int in) {
         if (!isFull())
             numbers[++total] = in;
