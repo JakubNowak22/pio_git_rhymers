4ca217b35a2a227924742f8d28e436ad31f054db
240763 <240763@edu.p.lodz.pl>
2023-04-24 19:33:40 +0200

    11 -Dokonano hermetyzacji nieprywatnych atrybutów

--- src/main/java/edu/kis/vh/nursery/list/Node.java
+++ src/main/java/edu/kis/vh/nursery/list/Node.java
@@ -9,4 +9,23 @@ public class Node {
         VALUE = i;
     }
 
+    public int getVALUE() {
+        return VALUE;
+    }
+
+    public Node getPrev() {
+        return prev;
+    }
+
+    public void setPrev(Node prev) {
+        this.prev = prev;
+    }
+
+    public Node getNext() {
+        return next;
+    }
+
+    public void setNext(Node next) {
+        this.next = next;
+    }
 }
